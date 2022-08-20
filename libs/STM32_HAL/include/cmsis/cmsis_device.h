b0dde3bcba774b8bffb9cb47d8acc77a286b69cf
PengJun <545681265@qq.com>
2022-08-21 00:20:08 +0800

    first commit

---
+++ libs/STM32_HAL/include/cmsis/cmsis_device.h
@@ -0,0 +1,15 @@
+#ifdef STM32F042x6
+#include <cmsis/device/stm32f042x6.h>
+#endif
+
+#ifdef STM32F072xB
+#include <cmsis/device/stm32f072xb.h>
+#endif
+
+#ifdef STM32F407xx
+#include <cmsis/device/stm32f407xx.h>
+#endif
+
+#ifdef STM32F412Rx
+#include <cmsis/device/stm32f412rx.h>
+#endif
